5438a4734d453cc7569a0dd7e7cc12650705b504
WinterPhoenix <winterphoenix96@protonmail.com>
2020-01-08 21:58:20 -0500

    WIP Linux and macOS support, Updated GenCEFCodecFix to exclude scenarios where the Original binary and the Fixed binary match, Fixed GenCEFCodecFix not automatically creating directories if they don't exist, Path concatenation now uses os.path.join to be more cross-platform friendly, Updated patches for Widevine support, and Updated patches for Proper User Agent support (will be fixed soon in GMod)

--- GenCEFCodecFix.py
+++ GenCEFCodecFix.py
@@ -1,56 +1,109 @@
-# Copyright 2019, Solstice Game Studios (www.solsticegamestudios.com)
+#!/usr/bin/env python3
+
+# GenCEFCodecFix: A manifest generation tool for GModCEFCodecFix
+#
+# NOTE: 64-bit required due to Memory requirements!
+#
+# Copyright 2020, Solstice Game Studios (www.solsticegamestudios.com)
 # LICENSE: GNU General Public License v3.0
 
 from time import process_time
+import os
 import bsdiff4
 from hashlib import sha256
 import json
 
 timeStart = process_time()
 
-originalPathRoot = r"D:\SteamLibrary\steamapps\common\GarrysMod"
-fixedPathRoot = r"D:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\addons\gmcl_shared_content\ChromiumH264\GarrysMod"
-patchTargetPathRoot = r"D:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\addons\sgs_gmodcefcodecfix\win32\x86-64"
+originalPathRoot = r"E:\cefcodecfix\release_original"
+fixedPathRoot = r"E:\cefcodecfix\release_fixed"
+patchTargetPathRoot = r"D:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\addons\sgs_gmodcefcodecfix"
 manifestDest = r"D:\SteamLibrary\steamapps\common\GarrysMod\garrysmod\addons\sgs_gmodcefcodecfix\manifest.json"
 
-# TODO: Support more than just x86-64 and Windows
-httpServerPathRoot = r"https://raw.githubusercontent.com/solsticegamestudios/GModCEFCodecFix/master/win32/x86-64"
+httpServerPathRoot = r"https://raw.githubusercontent.com/solsticegamestudios/GModCEFCodecFix/master"
 
-filesToDiff = [
-	r"bin\chrome_elf.dll",
-	r"bin\d3dcompiler_47.dll",
-	#"bin\icudtl.dat",
-	r"bin\libcef.dll",
-	r"bin\libEGL.dll",
-	r"bin\libGLESv2.dll",
-	#r"bin\natives_blob.bin",
-	#r"bin\snapshot_blob.bin",
-	#r"bin\v8_context_snapshot.bin",
+filesToDiff = {
+	"win32": {
+		"x86-64": [
+			r"bin\chrome_elf.dll",
+			r"bin\d3dcompiler_47.dll",
+			r"bin\icudtl.dat",
+			r"bin\libcef.dll",
+			r"bin\libEGL.dll",
+			r"bin\libGLESv2.dll",
+			r"bin\natives_blob.bin",
+			r"bin\snapshot_blob.bin",
+			r"bin\v8_context_snapshot.bin",
 
-	r"bin\chromium\cef.pak",
-	r"bin\chromium\cef_100_percent.pak",
-	r"bin\chromium\cef_200_percent.pak",
-	r"bin\chromium\cef_extensions.pak",
-	r"bin\chromium\devtools_resources.pak",
+			r"bin\chromium\cef.pak",
+			r"bin\chromium\cef_100_percent.pak",
+			r"bin\chromium\cef_200_percent.pak",
+			r"bin\chromium\cef_extensions.pak",
+			r"bin\chromium\devtools_resources.pak",
 
-	r"bin\win64\chrome_elf.dll",
-	r"bin\win64\d3dcompiler_47.dll",
-	#r"bin\win64\icudtl.dat"
-	r"bin\win64\libcef.dll",
-	r"bin\win64\libEGL.dll",
-	r"bin\win64\libGLESv2.dll",
-	#r"bin\win64\natives_blob.bin",
-	#r"bin\win64\snapshot_blob.bin",
-	#r"bin\win64\v8_context_snapshot.bin"
-]
+			r"bin\win64\chrome_elf.dll",
+			r"bin\win64\d3dcompiler_47.dll",
+			r"bin\win64\icudtl.dat",
+			r"bin\win64\libcef.dll",
+			r"bin\win64\libEGL.dll",
+			r"bin\win64\libGLESv2.dll",
+			r"bin\win64\natives_blob.bin",
+			r"bin\win64\snapshot_blob.bin",
+			r"bin\win64\v8_context_snapshot.bin"
+		]
+	},
+	"darwin": {
+		"x86-64": [
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Chromium Embedded Framework",
 
-print("Generating BSDIFF patches for " + str(len(filesToDiff)) + " files...")
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Libraries/libEGL.dylib",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Libraries/libGLESv2.dylib",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Libraries/libswiftshader_libEGL.dylib",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Libraries/libswiftshader_libGLESv2.dylib",
 
-for file in filesToDiff:
-	print("\t" + file)
-	fileTimeStart = process_time()
-	bsdiff4.file_diff(originalPathRoot + "\\" + file, fixedPathRoot + "\\" + file, patchTargetPathRoot + "\\" + file + ".bsdiff")
-	print("\t\tTook " + str(process_time() - fileTimeStart) + " second(s)")
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/cef.pak",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/cef_100_percent.pak",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/cef_200_percent.pak",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/cef_extensions.pak",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/devtools_resources.pak",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/icudtl.dat",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/natives_blob.bin",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/snapshot_blob.bin",
+			r"GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/v8_context_snapshot.bin"
+		]
+	},
+	"linux": {
+		"x86-64": [
+			r"bin/linux32/chromium/cef.pak",
+			r"bin/linux32/chromium/cef_100_percent.pak",
+			r"bin/linux32/chromium/cef_200_percent.pak",
+			r"bin/linux32/chromium/cef_extensions.pak",
+			r"bin/linux32/chromium/devtools_resources.pak",
+
+			#r"bin/linux64/chromium_process",
+			r"bin/linux64/icudtl.dat",
+			r"bin/linux64/libcef.so",
+			r"bin/linux64/libEGL.so",
+			r"bin/linux64/libGLESv2.so",
+			r"bin/linux64/natives_blob.bin",
+			r"bin/linux64/snapshot_blob.bin",
+			r"bin/linux64/v8_context_snapshot.bin"
+		]
+	}
+}
+
+print("Generating BSDIFF patches...")
+
+for platform in filesToDiff:
+	for branch in filesToDiff[platform]:
+		for file in filesToDiff[platform][branch]:
+			print("\t" + file)
+			fileTimeStart = process_time()
+
+			os.makedirs(os.path.dirname(os.path.join(patchTargetPathRoot, platform, branch, file)), exist_ok=True)
+			bsdiff4.file_diff(os.path.join(originalPathRoot , platform, file), os.path.join(fixedPathRoot, platform, file), os.path.join(patchTargetPathRoot, platform, branch, file + ".bsdiff"))
+
+			print("\t\tTook " + str(process_time() - fileTimeStart) + " second(s)")
 
 print("\nGenerating manifest.json...")
 manifestTimeStart = process_time()
@@ -68,20 +121,30 @@ def getFileSHA256(filePath):
 	return fileSHA256.hexdigest().upper()
 
 manifest = {}
-manifest["win32"] = {}
-manifest["win32"]["x86-64"] = {}
+for platform in filesToDiff:
+	manifest[platform] = platform in manifest and manifest[platform] or {}
 
-for file in filesToDiff:
-	manifest["win32"]["x86-64"][file] = {
-		"original": getFileSHA256(originalPathRoot + "\\" + file),
-		"patch": getFileSHA256(patchTargetPathRoot + "\\" + file + ".bsdiff"),
-		"patch-url": httpServerPathRoot + "/" + file.replace("\\", "/") + ".bsdiff",
-		"fixed": getFileSHA256(fixedPathRoot + "\\" + file)
-	}
+	for branch in filesToDiff[platform]:
+		manifest[platform][branch] = branch in manifest[platform] and manifest[platform][branch] or {}
+
+		for file in filesToDiff[platform][branch]:
+			originalHash = getFileSHA256(os.path.join(originalPathRoot, platform, file))
+			fixedHash = getFileSHA256(os.path.join(fixedPathRoot, platform, file))
+
+			if originalHash != fixedHash:
+				manifest[platform][branch][file] = {
+					"original": originalHash,
+					"patch": getFileSHA256(os.path.join(patchTargetPathRoot, platform, branch, file + ".bsdiff")),
+					"patch-url": httpServerPathRoot + "/" + platform + "/" + branch + "/" + file.replace("\\", "/") + ".bsdiff",
+					"fixed": fixedHash
+				}
+			else:
+				print("Warning: Original matches Fixed hash for " + file + ", removing...")
+				os.remove(os.path.join(patchTargetPathRoot, platform, branch, file + ".bsdiff"))
 
 with open(manifestDest, "w+") as manifestFile:
 	json.dump(manifest, manifestFile, indent=4)
 
 print("\tTook " + str(process_time() - manifestTimeStart) + " second(s)")
 
-print("\nCEFCodecFix Generation Complete, took " + str(process_time() - timeStart) + " second(s). NOTE: Remember to update the web server with all of this!")
+print("\nCEFCodecFix Generation Complete, took " + str(process_time() - timeStart) + " second(s). NOTE: Remember to update GitHub with all of this!")
