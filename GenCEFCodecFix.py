2dc85fb89b130b2ab4379784de5d0c45fb9dde06
WinterPhoenix <winterphoenix96@protonmail.com>
2023-05-25 17:23:43 -0400

    Gen: Update to support x86-64-temp branch (using x86-64's fixed files)

--- GenCEFCodecFix.py
+++ GenCEFCodecFix.py
@@ -153,6 +153,11 @@ for locale in locales:
 # macOS has gotta be special
 filesToDiff["darwin"]["x86-64"].append("GarrysMod_Signed.app/Contents/Frameworks/Chromium Embedded Framework.framework/Resources/en.lproj/locale.pak")
 
+# x86-64-temp is the same as x86-64, but with different original files
+filesToDiff["win32"]["x86-64-temp"] = filesToDiff["win32"]["x86-64"]
+filesToDiff["linux"]["x86-64-temp"] = filesToDiff["linux"]["x86-64"]
+filesToDiff["darwin"]["x86-64-temp"] = filesToDiff["darwin"]["x86-64"]
+
 print("\nArguments: " + str(sys.argv) + "\n")
 print("Original Path: " + originalPathRoot)
 print("Patch Path: " + patchTargetPathRoot + "\n")
@@ -194,7 +199,7 @@ for platform in filesToDiff:
 			fileHashes[platform][branch][file] = {}
 
 			originalFilePath = os.path.join(originalPathRoot, platform, branch, file)
-			fixedFilePath = os.path.join(fixedPathRoot, platform, branch, file)
+			fixedFilePath = os.path.join(fixedPathRoot, platform, "x86-64" if branch == "x86-64-temp" else branch, file) # x86-64-temp is the same as x86-64, but with different original files
 			patchFilePath = os.path.join(patchTargetPathRoot, platform, branch, file + ".bsdiff")
 
 			originalHash = getFileSHA256(originalFilePath)
