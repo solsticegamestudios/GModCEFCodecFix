2178af4842305c4720b82b90b0fb8b3e9185e546
WinterPhoenix <winterphoenix96@protonmail.com>
2024-02-21 03:31:57 -0500

    Update build scripts to CEF 120.0.6099.234

--- cef_build/linux.sh
+++ cef_build/linux.sh
@@ -4,4 +4,4 @@
 # NOTE: use_cups=false might be a little bit fucked. If CEF crashes after compiling with it, compile *without* it, then just overwrite libcef.so with the non-cups version
 
 export GN_DEFINES="is_official_build=true use_sysroot=true symbol_level=0 is_cfi=false use_thin_lto=false use_cups=false proprietary_codecs=true ffmpeg_branding=Chrome enable_widevine=true"
-/usr/bin/python3 ../automate/automate-git.py --build-target=cefsimple --download-dir=/home/winter/cefcodecfix/linux --force-build --x64-build --no-debug-build --force-distrib --client-distrib --no-distrib-archive --with-pgo-profiles --branch=5993 --chromium-checkout=refs/tags/118.0.5993.117
+/usr/bin/python3 ../automate/automate-git.py --build-target=cefsimple --download-dir=/home/winter/cefcodecfix/linux --force-build --x64-build --no-debug-build --force-distrib --client-distrib --no-distrib-archive --with-pgo-profiles --branch=6099 --chromium-checkout=refs/tags/120.0.6099.234
