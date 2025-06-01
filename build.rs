8444c38bd573553ed017b0f9853598f76dbbd059
John Peel <john@dgby.org>
2025-06-01 03:14:57 -0400

    Fixed build script on non-Windows platforms

--- build.rs
+++ build.rs
@@ -1,18 +1,13 @@
 // https://stackoverflow.com/a/65393488
-use {
-	std::{
-		env,
-		io,
-	},
-	winresource::WindowsResource,
-};
+use std::io;
 
 fn main() -> io::Result<()> {
-	if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-		WindowsResource::new()
-			// This path can be absolute, or relative to your crate root.
-			.set_icon("GModPatchToolLogo.ico")
-			.compile()?;
-	}
-	Ok(())
+    #[cfg(target_os = "windows")]
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        winresource::WindowsResource::new()
+            // This path can be absolute, or relative to your crate root.
+            .set_icon("GModPatchToolLogo.ico")
+            .compile()?;
+    }
+    Ok(())
 }
