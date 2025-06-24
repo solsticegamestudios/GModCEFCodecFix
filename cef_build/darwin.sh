6b2a10e8606f4683d8272eb25a9cb3899d18347d
WinterPhoenix <winterphoenix96@protonmail.com>
2025-06-24 01:05:52 -0400

    Update build scripts to CEF 137

--- cef_build/darwin.sh
+++ cef_build/darwin.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export GN_DEFINES="symbol_level=0 is_official_build=true proprietary_codecs=true ffmpeg_branding=Chrome enable_widevine=true"
-python3 ../automate/automate-git.py --download-dir=/Users/akiko/cefcodecfix/darwin --force-build --x64-build --no-debug-build --force-distrib --client-distrib --no-distrib-archive --with-pgo-profiles --branch=7103
+export GN_DEFINES="symbol_level=1 is_official_build=true proprietary_codecs=true ffmpeg_branding=Chrome enable_widevine=true"
+python3 ../automate/automate-git.py --download-dir=/Users/akiko/cefcodecfix/darwin --force-build --x64-build --no-debug-build --force-distrib --client-distrib --no-distrib-archive --with-pgo-profiles --branch=7151
