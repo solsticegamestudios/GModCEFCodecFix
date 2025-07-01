39166341b3ea3654906401af97a4d7e949d029d3
John Peel <john@dgby.org>
2025-06-30 21:57:50 -0400

    Build: Check if LFS files have been checked (#175)

--- build.rs
+++ build.rs
@@ -1,12 +1,28 @@
 // https://stackoverflow.com/a/65393488
-use std::io;
+use std::{
+	fs,
+	io::{self, Read},
+};
 
 fn main() -> io::Result<()> {
+	let mut buffer = [0; 8];
+	{
+		let mut file = fs::File::open("./GModPatchToolLogo.png")?;
+		file.read_exact(&mut buffer)?;
+	}
+	if buffer.starts_with(b"version ") {
+		println!("cargo::error=LFS files have not been checked out properly");
+	}
+
 	#[cfg(target_os = "windows")]
 	if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
 		// HACK: Since we aren't using SemVer "correctly" and FILEVERSION only supports 16 bits per version point, we've gotta break it out
 		// PRODUCTVERSION doesn't have the same limitation
-		let version = std::env::var("CARGO_PKG_VERSION_MAJOR").unwrap().parse().unwrap_or(0).to_string();
+		let version = std::env::var("CARGO_PKG_VERSION_MAJOR")
+			.unwrap()
+			.parse()
+			.unwrap_or(0)
+			.to_string();
 		let version_year = version[0..4].parse::<u64>().unwrap();
 		let version_month = version[4..6].parse::<u64>().unwrap();
 		let version_day = version[6..8].parse::<u64>().unwrap();
@@ -23,5 +39,6 @@ fn main() -> io::Result<()> {
 			.set_version_info(winresource::VersionInfo::FILEVERSION, version)
 			.compile()?;
 	}
+
 	Ok(())
 }
