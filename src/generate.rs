368ecaba0e9e124c47c1bf8c7d0747f237441802
WinterPhoenix <winterphoenix96@protonmail.com>
2025-07-05 20:44:39 -0400

    - Generate: Copy compressed versions of symbols to an output destination - Generate: Rename args to make more sense - Generate: Fix a few clippy issues

--- src/generate.rs
+++ src/generate.rs
@@ -10,16 +10,19 @@ use std::ops::Deref;
 #[command(version)]
 struct Args {
 	/// Path for Original files shipped with GMod
-	original_path: PathBuf,
+	original_src: PathBuf,
 
 	/// Path for Fixed (already-patched) files
-	fixed_path: PathBuf,
+	fixed_src: PathBuf,
 
 	/// Path for where to put the output Patch (bsdiff) files
-	patch_path: PathBuf,
+	patch_dest: PathBuf,
 
 	/// Path for where to copy the compressed versions of the Original files
-	original_compressed_path: PathBuf
+	original_dest: PathBuf,
+
+	/// Path for where to copy the compressed versions of the Symbol files
+	symbol_dest: PathBuf
 }
 
 fn get_files_recursive(source: &str, path_base: String, files: &mut HashMap<String, HashMap<String, PathBuf>>, dir_path: PathBuf) {
@@ -28,8 +31,10 @@ fn get_files_recursive(source: &str, path_base: String, files: &mut HashMap<Stri
 		let entry_path = entry.path();
 		let entry_filename = entry.file_name().into_string().unwrap();
 
-		// TODO: Move gmod-update.txt and .sym files from these directories
-		if entry_filename != "gmod-update.txt" && !entry_filename.contains(".sym") {
+		// TODO: Move gmod-update.txt from these directories
+		if entry_filename != "gmod-update.txt" {
+			let source = if entry_filename.contains(".sym") { "symbol" } else { source };
+			let entry_filename = if entry_filename.contains(".sym") { entry_filename.replace(".sym", "") } else { entry_filename };
 			let entry_relative_path_str = if path_base.is_empty() { entry_filename } else { format!("{path_base}/{entry_filename}") };
 
 			if entry_path.is_dir() {
@@ -52,14 +57,15 @@ fn get_files_recursive(source: &str, path_base: String, files: &mut HashMap<Stri
 	}
 }
 
-fn hash_diff_compress_file(patch_path: PathBuf, filename: &String, file_paths: &HashMap<String, PathBuf>, original_compressed_path: PathBuf) -> Result<(f64, HashMap<String, String>), (bool, String)> {
+fn hash_diff_compress_file(patch_dest: PathBuf, filename: &String, file_paths: &HashMap<String, PathBuf>, original_dest: PathBuf, symbol_dest: PathBuf) -> Result<(f64, HashMap<String, String>), (bool, String)> {
 	let now = Instant::now();
 	let mut hashes: HashMap<String, String> = HashMap::new();
 
-	let original_path = file_paths.get("original");
-	let fixed_path = file_paths.get("fixed");
-	let original_hash = if let Some(original_path) = original_path { get_file_hash(original_path) } else { Ok("null".to_string()) };
-	let fixed_hash = if let Some(fixed_path) = fixed_path { get_file_hash(fixed_path) } else { Ok("null".to_string()) };
+	let original_src = file_paths.get("original");
+	let fixed_src = file_paths.get("fixed");
+	let symbol_src = file_paths.get("symbol");
+	let original_hash = if let Some(original_src) = original_src { get_file_hash(original_src) } else { Ok("null".to_string()) };
+	let fixed_hash = if let Some(fixed_src) = fixed_src { get_file_hash(fixed_src) } else { Ok("null".to_string()) };
 
 	if let Err(original_hash) = original_hash {
 		return Err((true, original_hash));
@@ -79,8 +85,8 @@ fn hash_diff_compress_file(patch_path: PathBuf, filename: &String, file_paths: &
 	// Skip entirely if the "fixed" version is just deleting the file
 	// If the original file doesn't exist, we "generate" the patch against an empty file
 	if fixed_hash != "null" {
-		let original = if let Some(original_path) = original_path { std::fs::read(original_path) } else { Ok(Vec::new()) };
-		let fixed = if let Some(fixed_path) = fixed_path { std::fs::read(fixed_path) } else { Ok(Vec::new()) };
+		let original = if let Some(original_src) = original_src { std::fs::read(original_src) } else { Ok(Vec::new()) };
+		let fixed = if let Some(fixed_src) = fixed_src { std::fs::read(fixed_src) } else { Ok(Vec::new()) };
 
 		if let Err(original) = original {
 			return Err((true, original.to_string()));
@@ -131,7 +137,7 @@ fn hash_diff_compress_file(patch_path: PathBuf, filename: &String, file_paths: &
 		// Copy patch to patch file
 		let filename = format!("{filename}.bsdiff");
 		let file_parts: Vec<&str> = filename.split("/").collect();
-		let patch_file_path = extend_pathbuf_and_return(patch_path, &file_parts[..]);
+		let patch_file_path = extend_pathbuf_and_return(patch_dest, &file_parts[..]);
 		let mut patch_file_path_dir = patch_file_path.clone();
 		patch_file_path_dir.pop();
 
@@ -165,12 +171,12 @@ fn hash_diff_compress_file(patch_path: PathBuf, filename: &String, file_paths: &
 
 	// Create a compressed copy of the original file
 	if original_hash != "null" {
-		let original_path = original_path.unwrap();
+		let original_src = original_src.unwrap();
 		let filename = format!("{filename}.zst");
 		let file_parts: Vec<&str> = filename.split("/").collect();
-		let original_compressed_file_path = extend_pathbuf_and_return(original_compressed_path, &file_parts[..]);
+		let original_compressed_file_path = extend_pathbuf_and_return(original_dest, &file_parts[..]);
 
-		let original_file = std::fs::OpenOptions::new().read(true).open(original_path);
+		let original_file = std::fs::OpenOptions::new().read(true).open(original_src);
 		if let Err(original_file) = original_file {
 			return Err((true, original_file.to_string()));
 		}
@@ -197,8 +203,35 @@ fn hash_diff_compress_file(patch_path: PathBuf, filename: &String, file_paths: &
 	}
 
 	// Create compressed copies of fixed symbols
-	if fixed_hash != "null" {
-		// TODO
+	if let Some(symbol_src) = symbol_src {
+		let filename = format!("{filename}.sym.zst");
+		let file_parts: Vec<&str> = filename.split("/").collect();
+		let symbol_compressed_file_path = extend_pathbuf_and_return(symbol_dest, &file_parts[..]);
+
+		let symbol_file = std::fs::OpenOptions::new().read(true).open(symbol_src);
+		if let Err(symbol_file) = symbol_file {
+			return Err((true, symbol_file.to_string()));
+		}
+		let symbol_file = symbol_file.unwrap();
+
+		let mut symbol_compressed_file_path_dir = symbol_compressed_file_path.clone();
+		symbol_compressed_file_path_dir.pop();
+
+		let create_dir_result = std::fs::create_dir_all(symbol_compressed_file_path_dir);
+		if let Err(create_dir_result) = create_dir_result {
+			return Err((true, create_dir_result.to_string()));
+		}
+
+		let symbol_file_compressed = std::fs::OpenOptions::new().write(true).create(true).truncate(true).open(symbol_compressed_file_path);
+		if let Err(symbol_file_compressed) = symbol_file_compressed {
+			return Err((true, symbol_file_compressed.to_string()));
+		}
+		let symbol_file_compressed = symbol_file_compressed.unwrap();
+
+		let compress_result = zstd::stream::copy_encode(symbol_file, symbol_file_compressed, 0);
+		if let Err(compress_result) = compress_result {
+			return Err((true, compress_result.to_string()));
+		}
 	}
 
 	hashes.insert("original".to_string(), original_hash);
@@ -210,105 +243,128 @@ fn hash_diff_compress_file(patch_path: PathBuf, filename: &String, file_paths: &
 pub fn main() {
 	let now = Instant::now();
 
-	println!("{}", ABOUT);
+	println!("{ABOUT}");
 
 	// Parse the args (will also exit if something's wrong with them)
 	let args = Args::parse();
 
-	let original_path = pathbuf_to_canonical_pathbuf(args.original_path.clone(), true);
-	let fixed_path = pathbuf_to_canonical_pathbuf(args.fixed_path.clone(), true);
-	let patch_path = pathbuf_to_canonical_pathbuf(args.patch_path.clone(), false);
-	let original_compressed_path = pathbuf_to_canonical_pathbuf(args.original_compressed_path.clone(), false);
+	let original_src = pathbuf_to_canonical_pathbuf(args.original_src.clone(), true);
+	let fixed_src = pathbuf_to_canonical_pathbuf(args.fixed_src.clone(), true);
+	let patch_dest = pathbuf_to_canonical_pathbuf(args.patch_dest.clone(), false);
+	let original_dest = pathbuf_to_canonical_pathbuf(args.original_dest.clone(), false);
+	let symbol_dest = pathbuf_to_canonical_pathbuf(args.symbol_dest.clone(), false);
 
 	let mut cmd = Args::command();
-	if let Err(original_path) = original_path {
+	if let Err(original_src) = original_src {
 		cmd.error(
 			ErrorKind::InvalidValue,
-			format!("Original Path (Input): {original_path}"),
+			format!("Original Path (Input): {original_src}"),
 		)
 		.exit();
 	}
 
-	let original_path = original_path.unwrap();
-	let original_path_str = original_path.to_string_lossy();
-	println!("Original Path (Input): {}\n", original_path_str);
+	let original_src = original_src.unwrap();
+	let original_src_str = original_src.to_string_lossy();
+	println!("Original Path (Input): {original_src_str}\n");
 
-	if let Err(fixed_path) = fixed_path {
+	if let Err(fixed_src) = fixed_src {
 		cmd.error(
 			ErrorKind::InvalidValue,
-			format!("Fixed Path (Input): {fixed_path}"),
+			format!("Fixed Path (Input): {fixed_src}"),
 		)
 		.exit();
 	}
 
-	let fixed_path = fixed_path.unwrap();
-	let fixed_path_str = fixed_path.to_string_lossy();
-	println!("Fixed Path (Input): {}\n", fixed_path_str);
+	let fixed_src = fixed_src.unwrap();
+	let fixed_src_str = fixed_src.to_string_lossy();
+	println!("Fixed Path (Input): {fixed_src_str}\n");
 
-	if let Err(patch_path) = patch_path {
+	if let Err(patch_dest) = patch_dest {
 		cmd.error(
 			ErrorKind::InvalidValue,
-			format!("Patch Path (Output): {patch_path}"),
+			format!("Patch Path (Output): {patch_dest}"),
 		)
 		.exit();
 	}
 
-	let patch_path = patch_path.unwrap();
-	let patch_path_str = patch_path.to_string_lossy();
-	println!("Patch Path (Output): {}\n", patch_path_str);
+	let patch_dest = patch_dest.unwrap();
+	let patch_dest_str = patch_dest.to_string_lossy();
+	println!("Patch Path (Output): {patch_dest_str}\n");
 
-	if let Err(original_compressed_path) = original_compressed_path {
+	if let Err(original_dest) = original_dest {
 		cmd.error(
 			ErrorKind::InvalidValue,
-			format!("Original Compressed Path (Output): {original_compressed_path}"),
+			format!("Original Compressed Path (Output): {original_dest}"),
 		)
 		.exit();
 	}
 
-	let original_compressed_path = original_compressed_path.unwrap();
-	let original_compressed_path_str = original_compressed_path.to_string_lossy();
-	println!("Original Compressed Path (Output): {}\n", original_compressed_path_str);
+	let original_dest = original_dest.unwrap();
+	let original_dest_str = original_dest.to_string_lossy();
+	println!("Original Compressed Path (Output): {original_dest_str}\n");
 
-	if original_path == fixed_path {
+	if let Err(symbol_dest) = symbol_dest {
+		cmd.error(
+			ErrorKind::InvalidValue,
+			format!("Symbol Path (Output): {symbol_dest}"),
+		)
+		.exit();
+	}
+
+	let symbol_dest = symbol_dest.unwrap();
+	let symbol_dest_str = symbol_dest.to_string_lossy();
+	println!("Symbol Path (Output): {symbol_dest_str}\n");
+
+	if original_src == fixed_src {
 		cmd.error(
 			ErrorKind::ValueValidation,
-			"Original Path cannot match Fixed Path.",
+			"Original Source cannot match Fixed Source.",
 		)
 		.exit();
 	}
 
-	if original_compressed_path == fixed_path {
+	if original_dest == fixed_src {
 		cmd.error(
 			ErrorKind::ValueValidation,
-			"Original Compressed Path cannot match Fixed Path.",
+			"Original Dest cannot match Fixed Source.",
 		)
 		.exit();
 	}
 
-	let mut manifest_file_path = patch_path.clone();
+	let mut manifest_file_path = patch_dest.clone();
 	manifest_file_path.pop();
 	let manifest_file_path = extend_pathbuf_and_return(manifest_file_path, &["manifest.json"]);
 
 	println!("Deleting Old Patches Dir, Compressed Original Dir, and Manifest...");
 
-	let remove_result = std::fs::remove_dir_all(&patch_path);
+	let remove_result = std::fs::remove_dir_all(&patch_dest);
 	if let Err(remove_result) = remove_result {
 		println!("Failed to remove old patches dir: {remove_result}");
 	}
 
-	let create_result = std::fs::create_dir(&patch_path);
+	let create_result = std::fs::create_dir(&patch_dest);
 	if let Err(create_result) = create_result {
 		println!("Failed to create new patches dir: {create_result}");
 	}
 
-	let remove_result = std::fs::remove_dir_all(&original_compressed_path);
+	let remove_result = std::fs::remove_dir_all(&original_dest);
 	if let Err(remove_result) = remove_result {
 		println!("Failed to remove old original compressed dir: {remove_result}");
 	}
 
-	let create_result = std::fs::create_dir(&original_compressed_path);
+	let create_result = std::fs::create_dir(&original_dest);
 	if let Err(create_result) = create_result {
 		println!("Failed to create new original compressed dir: {create_result}");
+	}
+
+	let remove_result = std::fs::remove_dir_all(&symbol_dest);
+	if let Err(remove_result) = remove_result {
+		println!("Failed to remove old symbol dir: {remove_result}");
+	}
+
+	let create_result = std::fs::create_dir(&symbol_dest);
+	if let Err(create_result) = create_result {
+		println!("Failed to create new symbol dir: {create_result}");
 	}
 
 	let remove_result = std::fs::remove_file(&manifest_file_path);
@@ -319,13 +375,13 @@ pub fn main() {
 	println!("\n*** GENERATING PATCH FILES ***\n");
 
 	let mut files: HashMap<String, HashMap<String, PathBuf>> = HashMap::new();
-	get_files_recursive("original", "".to_string(), &mut files, original_path);
-	get_files_recursive("fixed", "".to_string(), &mut files, fixed_path);
+	get_files_recursive("original", "".to_string(), &mut files, original_src);
+	get_files_recursive("fixed", "".to_string(), &mut files, fixed_src);
 
 	let manifest: Mutex<Manifest> = Mutex::new(HashMap::new());
 
 	files.par_iter().for_each(|(filename, file_paths)| {
-		let result = hash_diff_compress_file(patch_path.clone(), filename, file_paths, original_compressed_path.clone());
+		let result = hash_diff_compress_file(patch_dest.clone(), filename, file_paths, original_dest.clone(), symbol_dest.clone());
 
 		match result {
 			Ok((time, hashes)) => {
